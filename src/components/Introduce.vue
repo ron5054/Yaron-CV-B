799c938f96735bbc9abf6f7f0618b7c59bf104d0
Yaron Levy <ron5054@gmail.com>
2023-08-20 12:35:18 +0300

    Add medium media query

--- src/components/Introduce.vue
+++ src/components/Introduce.vue
@@ -1,6 +1,6 @@
 <template>
     <section class="introduce-container" id="home">
         <h1>Hi, I am <span>Yaron</span>, <br> Frontend/Full Stack <br>Developer</h1>
-        <small> I code beatifully simple things and i love what i do.<br>Just simple like that!</small>
+        <small> I make beatifully simple code and I love what I do.<br>As simple as that!</small>
     </section>
 </template>
